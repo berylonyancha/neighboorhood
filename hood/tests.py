a56a2e1c78df1ec034087cf288d41d164f1c5697
Beryl Onyancha <berylonyancha@gmail>
2019-08-14 21:27:13 +0300

    Add tests for the profile class

---
+++ hood/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile,Post,Comment,Neighborhood,Company
+# Create your tests here.
+class UserProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = UserProfile(id=1,first_name='John',last_name='Doe',user = self.user,bio='test bio')
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,UserProfile))
+
